5fa04c0678edf8e885b5f594d21753ec0232093c
Hryhorii Tereshchenko <hryhoriit@ext.inditex.com>
2023-10-22 21:30:39 +0300

    initial commit

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mt-7 max-w-sm mx-auto text-center card">
+    <p class="mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
+    <p class="mt-7 text-6xl">Oops.</p>
+    <p class="mt-7">{{ error.message }}</p>
+    <button class="btn py-7" @click="handleClearError">Go Home</button>
+  </div>
+</template>
+
+<script setup>
+const handleClearError = () => clearError({ redirect: '/' });
+const { error } = defineProps(['error']);
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste hic
+      voluptate saepe, quas nulla facere officia at expedita quaerat esse earum
+      quidem, omnis, atque animi. Aliquam inventore est aut facilis!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste hic
+      voluptate saepe, quas nulla facere officia at expedita quaerat esse earum
+      quidem, omnis, atque animi. Aliquam inventore est aut facilis!
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
